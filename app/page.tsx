d9d13593f7342a144a05d497209b370d94b175bd
surveybaja-source <survey.baja@gmail.com>
2026-03-15 13:06:05 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useState } from 'react'
+
+export default function Home() {
+  return (
+    <div style={{minHeight:'100vh',background:'#0c1a27',display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <div style={{textAlign:'center'}}>
+        <h1 style={{color:'#dd2e1e',fontFamily:'sans-serif',fontSize:48,fontWeight:900}}>
+          Survey<span style={{color:'#ffffff'}}>Link</span>
+        </h1>
+        <p style={{color:'#8fa8c0',marginTop:16}}>Marine Cargo Survey Platform</p>
+      </div>
+    </div>
+  )
+}
